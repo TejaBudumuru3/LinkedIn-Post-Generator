d92e9ad49f028178d503f4b4348f21f4573daf53
vamsi-o <iamvamsi0@gmail.com>
2025-06-22 15:52:25 +0530

    First Commit

--- server/index.js
+++ server/index.js
@@ -1,11 +1,28 @@
 const express = require("express");
 require("dotenv").config();
 const cors = require("cors")
-
+const  {GoogleGenAI} =  require( '@google/genai')
 const app = express()
 
 app.use(cors());
 app.use(express.json())
+
+
+
+const GEMINI_API_KEY = "AIzaSyBS4dvqMniE_9Pg50lrLCQuz4ULGfr4VP4";
+
+const ai = new GoogleGenAI({apiKey: GEMINI_API_KEY});
+
+async function main() {
+  const response = await ai.models.generateContent({
+    model: 'gemini-2.0-flash-001',
+    contents: 'Why is the sky blue?',
+  });
+  console.log(response.text);
+}
+
+main();
+
 
 app.get("/",(req,res)=>{
     res.send("server running")

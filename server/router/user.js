ee937bb5f594e0019951facc54d2ca3845d3c64f
vamsi-o <iamvamsi0@gmail.com>
2025-06-23 17:54:35 +0530

    REgister end point Done

--- server/router/user.js
+++ server/router/user.js
@@ -1,31 +1,38 @@
 
 const { Router } = require('express');
-const {UserModel} = require('../models/userSchema');
+const { UserModel } = require('../models/userSchema');
 const bcrypt = require('bcryptjs');
+const { message } = require('statuses');
 // const UserModel = require("../mode")
 
 const UserRouter = Router();
-UserRouter.post("/register", async (req,res)=>{
+UserRouter.post("/register", async (req, res) => {
     const { name, email, password } = req.body;
     try{
-        const userMail = await UserModel.find({ email });
-        if(userMail){
-            return res.status(400).json({ message: "user already exists" });
-        }
-        else{
-            const hashedPassword = await bcrypt.hash(password, 10);
-            UserModel.create({
-                username: name,
-                email,
-                password : hashedPassword
-            })
-            console.log("Account sign up successfull ")
-            return res.status(201).json({ message: "user created successfully" });
-        }
+    const EmailCheck = await UserModel.findOne({
+        email
+    })
+    if (EmailCheck) {
+       return  res.status(400).json(
+            {
+                message: "email alreay exsists try with a different one "
+            }
+        )
     }
-    catch(e){
-        console.log(e)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    UserModel.create({
+        username: name,
+        email,
+        password: hashedPassword
+    })
+    // console.log("Account sign up successfull ")
+     return res.status(201).json({ message: "user created successfully" });
+    }catch(error)
+    {
+        console.log(error)
     }
+
+
 })
 
-module.exports = {UserRouter};
+module.exports = { UserRouter };
